b15f324cef88af16ee4c3d872f0019188784ac87
BlueOink <62630348+BlueOink@users.noreply.github.com>
2020-03-26 21:44:07 -0400

    Fixed Positions Stream
    Todo: Make it look pretty

--- options.py
+++ options.py
@@ -2,6 +2,7 @@ import os.path
 import praw
 import re
 import sys
+import time
 
 
 def clear():
@@ -12,20 +13,9 @@ def write(str):
 	clear()
 	sys.stdout.write(str+' \r')
 	sys.stdout.flush()
-	
-	#print('\n')
 
-
-def update(cp,pp, pos, posl):
-	
-	
-#	if posl is None
-	p = 'call: %s %% put: %s %%'  % (cp, pp)
-	dash = '-' *20
-	
-	pos = pos + '\n'
-	
-	print('', end = '\r', flush = True)
+def getHourly():
+	epoch = int(time.time())
 	
 
 # bot login credentials
@@ -36,8 +26,8 @@ password = ''
 
 # Your app details
 user_agent = 'bot'
-client_id = 'E2efw'
-client_secret = 'rU'
+client_id = ''
+client_secret = ''
 
 reddit = praw.Reddit(
     client_id=client_id,
@@ -48,11 +38,9 @@ reddit = praw.Reddit(
 
 subreddit = reddit.subreddit('wallstreetbets')
 
-#if not os.path.exists('words.txt'):
-#	f = open('words.txt', 'w+')
-#	f.close()
 put = 1
 call = 1
+
 
 print('_-OPTIONS-_')
 for c in subreddit.stream.comments():
@@ -60,36 +48,40 @@ for c in subreddit.stream.comments():
 	body = c.body.lower()
 	total = put + call
 	out = " put: " + str(round(put/total*100,2))+"% call: " + str(round(call/total*100,2)) + '%' 
-	#print(out)
+	
 
 
 	if "put" in body:
 		put = put+1
 		print(out, end = '\r', flush = True)
-		#print(c.body)
+		
 		
 		
 	elif "call" in body:
 		call = call + 1
 		print(out, end = '\r', flush = True)
 	
-		#print('----')
-		#print(c.body)
 		
 	elif '/' in body:
-		strike = re.search(r'\d{1,5}[cp]', body)
-		date = re.search(r'\d{1,2}/\d{1,2}', body)
-		ticker = re.search(r'\w{1,5}',body)
+		
+		ticker = re.search(r'[A-Z]{1,5} ', c.body)
 		pos_tsd = re.search(r'\w{1,5} \d{1,5}[cp] \d{1,2}/\d{1,2}', body)
 		pos_tds = re.search(r'\w{1,5} \d{1,2}/\d{1,2} \d{1,5}[cp]', body)
-		#print('.')
+	#	print('.')
 		if pos_tds is not None:
-		#	print(' ' * len(out))
+			print(' ' * len(out) , end = '  \r')
 			#print('%s %s %s' % (str(ticker.group(0)), strike.group(0), date.group(0)))
-			print(pos_tds.group(0))
+			if ticker is not None:
+				print('Ticker: '+ticker.group(0))
+				
+			print(pos_tds .group(0) +'\n'+('-'*20)+'\n' +body )
+			print('-'*20)
 		elif pos_tsd is not None:
-		#	print(' ' * len(out))
+			print(' ' * len(out), end = '  \r')
 			#print('%s %s %s' % (str(ticker.group(0)), strike.group(0), date.group(0)))
-			print(pos_tsd .group(0))
+			if ticker is not None:
+				print('Ticker: '+ticker.group(0))
+			print(pos_tsd .group(0) +'\n'+('-'*20)+'\n' +body)
+			print('-' *20)
 		
 #	print('\n')
