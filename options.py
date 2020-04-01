9efa64222407525d4761c3f8665cf17cdcadbdfc
BlueOink <62630348+BlueOink@users.noreply.github.com>
2020-04-01 08:29:22 -0400

    Optimize efficiency
    Instead of a stream it scans post history and saves positions to a csv file. Just needs some cleaning up.

--- options.py
+++ options.py
@@ -17,6 +17,13 @@ def write(str):
 def getHourly():
 	epoch = int(time.time())
 	
+def writefile(txt, fn):
+	if not os.path.exists(fn):
+		f = open(fn, 'w+')
+		f.close()
+	f = open(fn,'a')
+	f.write(txt)
+	f.close()
 
 # bot login credentials
 username = ''
@@ -42,47 +49,57 @@ put = 1
 call = 1
 
 
+put = 1
+call = 1
+i = 0
 print('_-OPTIONS-_')
-for c in subreddit.stream.comments():
-	
-	body = c.body.lower()
-	total = put + call
-	out = " put: " + str(round(put/total*100,2))+"% call: " + str(round(call/total*100,2)) + '%' 
+for submission in subreddit.new(limit=999):
+	epoch = int(time.time())
+	if (epoch - submission.created_utc)/60/60 >= 24:
+		break
+	submission.comments.replace_more()
+	for c in submission.comments.list():
+
+		body = c.body.lower()
+
+		
+		total = put + call
+		out = " put: " + str(round(put/total*100,2))+	"% call: " + str(round(call/total*100,2)) + '%' 
 	
 
 
-	if "put" in body:
-		put = put+1
-		print(out, end = '\r', flush = True)
+		if "put" in body:
+			put = put+1
+			print(out, end = '\r', flush = True)
 		
 		
 		
-	elif "call" in body:
-		call = call + 1
-		print(out, end = '\r', flush = True)
+		elif "call" in body:
+			call = call + 1
+			print(out, end = '\r', flush = True)
 	
 		
-	elif '/' in body:
+		elif '/' in body:
 		
-		ticker = re.search(r'[A-Z]{1,5} \d', c.body)
-		pos_tsd = re.search(r'[A-Z]{1,5} \d{1,5}[cp] \d{1,2}/\d{1,2}', c.body)
-		pos_tds = re.search(r'[A-Z]{1,5} \d{1,2}/\d{1,2} \d{1,5}[cp]', c.body)
+			ticker = re.search(r'[A-Z]{1,5} \d', c.body)
+			pos_tsd = re.search(r'[A-Z]{1,5} \d{1,5}[cp] \d{1,2}/\d{1,2}', c.body)
+			pos_tds = re.search(r'[A-Z]{1,5} \d{1,2}/\d{1,2} \d{1,5}[cp]', c.body)
 	#	print('.')
-		if pos_tds is not None:
-			print(' ' * len(out) , end = '  \r')
+			if pos_tds is not None:
+				print(' ' * len(out) , end = '  \r')
 			#print('%s %s %s' % (str(ticker.group(0)), strike.group(0), date.group(0)))
-			if ticker is not None:
+				if ticker is not None:
 				#print('Ticker: '+ticker.group(0))
-				pass
-			print(pos_tds .group(0))
-			print('-'*20)
-		elif pos_tsd is not None:
-			print(' ' * len(out), end = '  \r')
+					pass
+				print(pos_tds .group(0))
+				writefile(pos_tds.group(0)+',', 'positions.csv')
+				print('-'*20)
+			elif pos_tsd is not None:
+				print(' ' * len(out), end = '  \r')
 			#print('%s %s %s' % (str(ticker.group(0)), strike.group(0), date.group(0)))
-			if ticker is not None:
+				if ticker is not None:
 				#print('Ticker: '+ticker.group(0)
-				pass
-			print(pos_tsd .group(0))
-			print('-' *20)
-		
-#	print('\n')
+					pass
+				print(pos_tsd .group(0))
+				writefile(pos_tsd.group(0) +',', 'positions.csv')
+				print('-' *20)
